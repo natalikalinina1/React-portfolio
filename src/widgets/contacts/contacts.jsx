fa29afe176a66ff1ca9d642b83865ddb3b629ea0
natalikalinina1 <kidmil@mail.ru>
2023-08-01 20:06:01 +0300

    react-smooth-scroll

---
+++ src/widgets/contacts/contacts.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import { Element } from "react-scroll";
+import "./contacts.css";
+
+export const Contacts = () =>{
+    return (
+
+        <Element name = "contacts" className="contacts">
+          Contacts
+        </Element>
+    );
+};
---
+++ src/widgets/footer/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () =>{
+    return <div className="footer"> <p> 2023 All Rights Reserved.</p> </div>
+
+}
+
